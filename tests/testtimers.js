c1e9326418372724a31aa416926eb3bbe13f97b4
Jesus Ruiz <jr3791@live.com>
2014-05-29 22:41:40 -0700

    One more timer test

--- tests/testtimers.js
+++ tests/testtimers.js
@@ -16,9 +16,77 @@ d.run({
                 console.log('timeout @' + new Date());
             }
         }
+    },
+    timer2$state: {
+        input: {
+            review: {
+                when: { subject: 'approve' },
+                run: {
+                    first$state: {
+                        send: {
+                            start: {
+                                run: function (s) { s.startTimer('first', 4); },
+                                to: 'evaluate'
+                            }
+                        },
+                        evaluate: {
+                            wait: {
+                                when: { $t: 'first' },
+                                run: function (s) { s.signal({ id: 2, subject: 'approved' }); },
+                                to: 'end'
+                            }
+                        },
+                        end: {
+                        }
+
+                    },
+                    second$state: {
+                        send: {
+                            start: {
+                                run: function (s) { s.startTimer('second', 3); },
+                                to: 'evaluate'
+                            }
+                        },
+                        evaluate: {
+                            wait: {
+                                when: { $t: 'second' },
+                                run: function (s) { s.signal({ id: 3, subject: 'denied' }); },
+                                to: 'end'
+                            }
+                        },
+                        end: {
+                        }
+                    }
+                },
+                to: 'pending'
+            }
+        },
+        pending: {
+            approve: {
+                when: { subject: 'approved' },
+                run: function (s) { console.log('approved'); },
+                to: 'approved'
+            },
+            deny: {
+                when: { subject: 'denied' },
+                run: function (s) { console.log('denied'); },
+                to: 'denied'
+            }
+        },
+        denied: {
+        },
+        approved: {
+        }
     }
 }, '', null, function(host) {
     host.post('timer1', { id: 1, sid: 1, start: 'yes' }, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('ok');
+        }
+    });
+    host.post('timer2', { id: '1', sid: 1, subject: 'approve' }, function (err) {
         if (err) {
             console.log(err);
         } else {
