4f2c68fb6b2ecd4d5907cca9ea6a9b7955d116a3
Jesus Ruiz <jr3791@live.com>
2016-03-31 21:49:27 -0700

    fixing testdynamic

--- testpy/testdynamic.py
+++ testpy/testdynamic.py
@@ -4,6 +4,7 @@ import json
 from durable import engine
 from durable import interface
 from werkzeug.routing import Rule
+from werkzeug.wrappers import Request, Response
 from werkzeug.wsgi import SharedDataMiddleware
 
 
@@ -11,10 +12,11 @@ class Host(engine.Host):
 
     def __init__(self):
         super(Host, self).__init__()
-        self._redis_server = redis.Redis(unix_socket_path='/tmp/redis.sock')
+        self._redis_server = redis.Redis()
 
     def get_action(self, action_name):
-        def print_lamba(state):
+        def print_lamba(c):
+            c.s.label = action_name
             print('Executing {0}'.format(action_name))
 
         return print_lamba;
@@ -34,15 +36,24 @@ class Host(engine.Host):
 class Application(interface.Application):
 
     def __init__(self, host):
-        super(Application, self).__init__(host, [Rule('/testdynamic.html', endpoint=self._dynamic_request)])
+        super(Application, self).__init__(host, '127.0.0.1', 5000, [Rule('/testdynamic.html', endpoint=self._dynamic_request), 
+                                                                    Rule('/durableVisual.js', endpoint=self._dynamic_request), 
+                                                                    Rule('/<ruleset_name>/<sid>/admin.html', endpoint=self._admin_request), 
+                                                                    Rule('/favicon.ico', endpoint=self._empty)])
+
+    def _empty(self, environ, start_response):
+        return Response()(environ, start_response)
 
     def _not_found(self, environ, start_response):
         return Exception('File not found')
 
     def _dynamic_request(self, environ, start_response):
-        middleware = SharedDataMiddleware(self._not_found, {'/': os.path.dirname(__file__)})
+        middleware = SharedDataMiddleware(self._not_found, {'/': 'testjs'})
         return middleware(environ, start_response)
 
+    def _admin_request(self, environ, start_response, ruleset_name, sid):
+        middleware = SharedDataMiddleware(self._not_found, {'/{0}/{1}/'.format(ruleset_name, sid): 'testjs'})
+        return middleware(environ, start_response)
 
 if __name__ == '__main__':
     main_host = Host()
