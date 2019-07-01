9bea41a5a98e884a5407324ef0245d4790d68523
Jesus Ruiz <jr3791@live.com>
2019-06-30 17:50:35 -0700

    Bumping version

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
 
 setup (
     name = 'durable_rules',
-    version = '0.33.134',
+    version = '0.33.135',
     description = 'for real time analytics (a Python Rules Engine)',
     long_description=long_description,
     url='https://github.com/jruizgit/rules',
